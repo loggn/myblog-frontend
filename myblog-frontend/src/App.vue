4dd3f025622c82c404acca899f6062e867130689
loggn <1137127358@qq.com>
2025-08-16 00:13:18 +0800

    创建了界面，设置了路由

--- myblog-frontend/src/App.vue
+++ myblog-frontend/src/App.vue
@@ -1,7 +1,16 @@
 <script setup></script>
 
 <template>
-  <h1>欢迎光临~~！男宾一位里面请</h1>
+  <!-- 暂时方便保留，方便切换界面 -->
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/start">start</router-link> |
+    <router-link to="/articles">Articles</router-link> |
+    <router-link to="/articles/:id">ArticleDetail</router-link>
+  </nav>
+  <div>
+    <router-view />
+  </div>
 </template>
 
 <style scoped></style>
--- myblog-frontend/src/router/index.js
+++ myblog-frontend/src/router/index.js
@@ -1,8 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/views/HomePage.vue'
+import StartPage from '@/views/StartPage.vue'
+import ArticlesListPage from '@/views/ArticlesListPage.vue'
+import ArticleDetailPage from '@/views/ArticleDetailPage.vue'
+
+const routes = [
+  { path: '/', name: 'Home', component: HomePage },
+  { path: '/articles', name: 'Articles', component: ArticlesListPage },
+  { path: '/articles/:id', name: 'ArticleDetail', component: ArticleDetailPage },
+  { path: '/start', name: 'start', component: StartPage}
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
+})
+
+let first = true
+router.beforeEach((to, from, next) => {
+  if (to.path === '/' && first) {
+    first = false
+    next('/start')
+  } else {
+    next()
+  }
 })
 
 export default router
